dce3339d5983affd141cecbaac512e1f8afd7669
Sshiril <113382540+Sshiril@users.noreply.github.com>
2022-10-03 00:07:05 -0400

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,11 @@
+using System;
+
+//namespace hello{
+class Program {
+  public static void Main (string[] args) {
+
+    String name= Console.ReadLine();
+    Console.WriteLine (name);
+  }
+}
+//}
