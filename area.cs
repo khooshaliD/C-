f10e061ae61d3e9a4c2bd4f83cd72e29b9c508b4
Sshiril <113382540+Sshiril@users.noreply.github.com>
2022-10-07 14:48:07 -0400

    Create area.cs

---
+++ area.cs
@@ -0,0 +1,24 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    //int radius,radius1=8; // declaration
+    //double area,area1;
+    
+    String name;
+    name ="Shiril";
+    //const double pi=3.14;
+
+    //radius=5; //initialization
+    //area= pi*radius*radius;
+
+   // area1=pi*radius1*radius1;
+    
+    //Console.WriteLine ("Area is           "+area);// + is used to append the value 
+   // Console.WriteLine("Area is"+area1);
+    Console.Writeline(name);
+    
+    
+    
+  }
+}
