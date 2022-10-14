f04c6c427f434d017abda674fc82de055b672cce
Sshiril <113382540+Sshiril@users.noreply.github.com>
2022-10-14 12:35:39 -0400

    Update oct14.cs

--- oct14.cs
+++ oct14.cs
@@ -14,3 +14,32 @@ namespace Sample
 		}
 	}
 }
+/*
+using System;
+ 
+namespace Sample
+{
+	class Test
+	{
+		public static void Main(string[] args)
+		{
+			int firstNumber = 5, secondNumber = 10, result;
+			result = firstNumber + secondNumber;
+			Console.WriteLine("{0} + {1} = {2}", firstNumber, secondNumber, result);
+		}
+	}
+}*/
+/*
+using System;
+ 
+
+	class Test
+	{
+		public static void Main(string[] args)
+		{
+			string testString;
+			Console.Write("Enter a string - ");
+			testString = Console.ReadLine();
+			Console.WriteLine("You entered '{0}'", testString);
+		}
+	}*/
