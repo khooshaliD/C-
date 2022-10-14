6aa212e7913441b8da6c1df5eec2dd821e040d71
Sshiril <113382540+Sshiril@users.noreply.github.com>
2022-10-14 12:36:45 -0400

    Update oct14.cs

--- oct14.cs
+++ oct14.cs
@@ -31,6 +31,9 @@ namespace Sample
 }*/
 /*
 using System;
+
+
+
  
 
 	class Test
@@ -43,3 +46,30 @@ using System;
 			Console.WriteLine("You entered '{0}'", testString);
 		}
 	}*/
+/*
+using System;
+ 
+namespace UserInput
+{
+	class MyClass
+	{
+		public static void Main(string[] args)
+		{
+			string userInput;
+			int intVal;
+			double doubleVal;
+
+			Console.Write("Enter integer value: ");
+			userInput = Console.ReadLine();
+			/* Converts to integer type */
+			intVal = Convert.ToInt32(userInput);
+			Console.WriteLine("You entered {0}",intVal);
+
+			Console.Write("Enter double value: ");
+			userInput = Console.ReadLine();
+			/* Converts to double type */
+			doubleVal = Convert.ToDouble(userInput);
+			Console.WriteLine("You entered {0}",doubleVal);
+		}
+	}
+}*/
