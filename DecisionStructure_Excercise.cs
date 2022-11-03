1f620043551f084599c50a586c6bc666f09f33f0
khooshali7 <115113232+khooshali7@users.noreply.github.com>
2022-11-03 13:52:55 -0400

    Add files via upload

---
+++ DecisionStructure_Excercise.cs
@@ -0,0 +1,89 @@
+using System;
+public class DecisionStructure
+{
+    public static void Main(string[] args)
+    {
+        string ans;
+
+        Console.WriteLine("\n1.Decision Control Statement in C# can be implemented using");
+        Console.WriteLine("a)  if");
+        Console.WriteLine("b)  if-else");
+        Console.WriteLine("c)  conditional operator");
+        Console.WriteLine("d)  all of the above");
+
+        Console.Write("Answer:\t");
+        ans = Console.ReadLine();
+        if (ans == "d")
+        {
+            Console.WriteLine("Correct Answer");
+        }
+        else
+        {
+            Console.WriteLine("Incorrect Answer");
+        }
+        Console.WriteLine("\n2.If block always need to be associated with a else block");
+        Console.WriteLine("a)  True");
+        Console.WriteLine("b)  False");
+        Console.WriteLine("c)  NA");
+        Console.WriteLine("d)  Can't say");
+        Console.Write("Answer:\t");
+        ans = Console.ReadLine();
+        if (ans == "a")
+        {
+            Console.WriteLine("Correct Answer");
+        }
+        else
+        {
+            Console.WriteLine("Incorrect Answer");
+        }
+
+        Console.WriteLine("\n3.Which of the following is binary operator?");
+        Console.WriteLine("a)  &&");
+        Console.WriteLine("b)  ||");
+        Console.WriteLine("c)  both a and b");
+        Console.WriteLine("d)  !");
+        Console.Write("Answer:\t");
+        ans = Console.ReadLine();
+        if (ans == "d")
+        {
+            Console.WriteLine("Correct Answer");
+        }
+        else
+        {
+            Console.WriteLine("Incorrect Answer");
+        }
+
+        Console.WriteLine("\n4.A switch statement is used to?");
+        Console.WriteLine("a)  Switch between functions in a program");
+        Console.WriteLine("b)  Switch from one variable to another variable");
+        Console.WriteLine("c)  To choose from multiple possibilities which may arise due to different values of a single variable");
+        Console.WriteLine("d)  all of the above");
+        Console.Write("Answer:\t");
+        ans = Console.ReadLine();
+        if (ans == "c")
+        {
+            Console.WriteLine("Correct Answer");
+        }
+        else
+        {
+            Console.WriteLine("Incorrect Answer");
+        }
+        Console.WriteLine("\n5.If you have to make decision based on multiple choices, which of the following is best suited?");
+        Console.WriteLine("a)  if");
+        Console.WriteLine("b)  if-else");
+        Console.WriteLine("c)  if-else-if");
+        Console.WriteLine("d)  all of the above");
+        Console.Write("Answer:\t");
+        ans = Console.ReadLine();
+        if (ans == "c")
+        {
+            Console.WriteLine("Correct Answer");
+        }
+        else
+        {
+            Console.WriteLine("Incorrect Answer");
+        }
+    }
+}
+
+
